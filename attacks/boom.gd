1539d6dffaa19931c6fa9fc133074e2e1d67db3e
InnocentusLime <innocentuslime.help@gmail.com>
2026-06-06 13:44:40 +0300

    new meta: parryable bullets

--- attacks/boom.gd
+++ attacks/boom.gd
@@ -1,7 +1,7 @@
 class_name Boom
 extends Node2D
 
-@onready var hitbox = $Damge
+@onready var hitbox: Area2D = $Damge
 
 var processed: bool = false
 
@@ -16,7 +16,12 @@ func _physics_process(delta):
 	var to_dmg_list: Array[Node2D] = hitbox.get_overlapping_bodies()
 	for to_dmg in to_dmg_list:
 		if to_dmg.has_method("_on_dmg"):
-				print("calling")
 				to_dmg.call("_on_dmg", position)
+	
+	var to_dmg_list2: Array[Area2D] = hitbox.get_overlapping_areas()
+	for to_dmg in to_dmg_list2:
+		if to_dmg.has_method("_on_dmg"):
+				to_dmg.call("_on_dmg", position)
+				
 	processed = true
 	GameEvents.player_comboed.emit(to_dmg_list.size())
--- attacks/laserball.gd
+++ attacks/laserball.gd
@@ -3,6 +3,11 @@ extends Area2D
 
 var move_vel: Vector2 = Vector2(-15.0, 0.0)
 
+func _on_dmg(dmg_pos: Vector2):
+	var move_abs: float = move_vel.length()
+	var move_dir = (position - dmg_pos).normalized()
+	move_vel = move_dir * (move_abs * 0.6)
+
 func _ready():
 	GameEvents.game_over.connect(queue_free)
 
