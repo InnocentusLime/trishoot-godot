66cd7f30b11b27f589119e604d8ca38101b7a251
InnocentusLime <innocentuslime.help@gmail.com>
2026-09-13 16:06:51 +0300

    add minor aim assist to rockets

--- hazard/rocket.gd
+++ hazard/rocket.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 const PARRIED_SPEED: float = 400.0
 
+@export var assist_angle: float
 @export var rot_weight: float
 @export var move_vel: float
 @export var move_dir: Vector2
@@ -12,10 +13,28 @@ const PARRIED_SPEED: float = 400.0
 var parried: bool = false
 
 func _on_dmg(dmg_pos: Vector2, level: int) -> bool:
+	if parried: return false
 	if level < level_to_parry: return false
-	move_dir = (position - GameEvents.player_pos).normalized()
 	move_vel = PARRIED_SPEED
 	parried = true
+	
+	move_dir = (position - GameEvents.player_pos).normalized()
+	var rocketers := get_tree().get_nodes_in_group("rocketers")
+	if rocketers.is_empty(): return true
+	
+	var closest_reachable_rocketer_dist := INF
+	var closest_reachable_rocketer_dir := Vector2.ZERO
+	for rocketer in rocketers:
+		var rocketer_node: Node2D = rocketer
+		var dr_to_rocketer := rocketer_node.position - position
+		var reorient_angle := move_dir.angle_to(dr_to_rocketer)
+		if dr_to_rocketer.length() < closest_reachable_rocketer_dist and reorient_angle <= deg_to_rad(assist_angle):
+			closest_reachable_rocketer_dir = dr_to_rocketer.normalized()
+			closest_reachable_rocketer_dist = dr_to_rocketer.length()
+	if closest_reachable_rocketer_dist < INF:
+		print("redirecting")
+		move_dir = closest_reachable_rocketer_dir
+	
 	return true
 
 func _physics_process(delta):
